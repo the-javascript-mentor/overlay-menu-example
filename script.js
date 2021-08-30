588e9e7bf27e4ac5179c4158707e7977ebf45111
Bence A. Tóth <tothab@gmail.com>
2021-08-30 19:41:19 +0200

    Fixed scrolling issue

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const hamburger = document.getElementById("hamburger");
 const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   modal.classList.toggle("open");
+  if (modal.classList.contains("open")) {
+    // Only if the modal is fading in
+    body.classList.add("no-scroll");
+  }
+});
+
+overlay.addEventListener("transitionend", () => {
+  if (!modal.classList.contains("open")) {
+    // Only if the modal is fading out
+    body.classList.remove("no-scroll");
+  }
 });
